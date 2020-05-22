95dd2b73dd8aa4365a37c5cc77d5d4e3dd6f84f2
saket61195 <saket61195@gmail.com>
2020-05-22 18:05:15 +0530

    fetch data using postman

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,47 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello world");
+});
+
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
+
+exports.createScreams = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: `method not allowed ` });
+  }
+  const newScreams = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScreams)
+    .then((doc) => {
+      res.json({ message: `documents ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: `somthing went worng` });
+      console.error(err);
+    });
+});
